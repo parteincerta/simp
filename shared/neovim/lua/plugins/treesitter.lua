c3d488feab828f47b430fdd342845461e923759e
Fernando Moreira <fernando@parteincerta.com>
2025-01-05 04:01:18 +0000

    Set list of treesitter parsers to be pre-installed

--- shared/neovim/lua/plugins/treesitter.lua
+++ shared/neovim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 -- Language parser with semantic capabilites.
 -- https://github.com/nvim-treesitter/nvim-treesitter
 
-return { {
+return {
 	"nvim-treesitter/nvim-treesitter",
 	tag = "v0.9.3",
 	lazy = false,
@@ -11,7 +11,15 @@ return { {
 		require("nvim-treesitter.configs").setup({
 			auto_install = true,
 			sync_install = false,
-			ensure_installed = { "diff", "git_rebase" },
+			ensure_installed = {
+				"bash", "c", "cmake", "cpp", "diff", "dockerfile", "editorconfig",
+				"fish", "git_config", "git_rebase", "gitattributes", "gitcommit",
+				"gitignore", "gpg", "ini", "javascript", "json", "json5", "jsonc",
+				"lua", "make", "markdown", "markdown_inline", "nginx", "nix",
+				"pem", "powershell", "python", "query", "readline", "sql",
+				"ssh_config", "toml", "typescript", "vim", "vimdoc", "xml",
+				"yaml", "zig",
+			},
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = false,
@@ -25,4 +33,4 @@ return { {
 			},
 		})
 	end
-} }
+}
